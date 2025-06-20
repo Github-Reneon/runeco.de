e9af9e8105bf5f9b01453876c01e8bc21df8fb31
Andrew Lovick <Github-Reneon@users.noreply.github.com>
2025-06-20 14:29:15 +0100

    Update button container class name to horizontal_container in index.php

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
   <div class="jumbotron">
     <h1>runecode</h1>
     <h2><i>Welcome to the portal</i></h2>
-    <div class="button_container">
+    <div class="horizontal_container">
       <div class="button"><a href="">PF2e Party Planner</a></div>
       <div class="button"><a href="about.php">About</a></div>
     </div>
