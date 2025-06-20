9399a706a1d2bdb5bac87141e015d22dae711ddf
Andrew Lovick <Github-Reneon@users.noreply.github.com>
2025-06-20 13:44:29 +0100

    Refactor button layout in index.php and add button_container styles in style.css

--- src/index.php
+++ src/index.php
@@ -7,8 +7,10 @@
   <div class="jumbotron">
     <h1>runecode</h1>
     <h2><i>Welcome to the portal</i></h2>
-    <a class="button" href="">PF2e Party Planner</a>
-    <a class="button" href="">About</a>
+    <div class="button_container">
+      <div class="button"><a href="">PF2e Party Planner</a></div>
+      <div class="button"><a href="">About</a></div>
+    </div>
   </div>
 </body>
 
